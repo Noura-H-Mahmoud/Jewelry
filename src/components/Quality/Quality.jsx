47334126bba59da041514aa7a91d623733d29798
Noura-H-Mahmoud <nouramahmoud@Nouras-MacBook-Air.local>
2024-12-12 15:09:27 +0400

    aos  commit

--- src/components/Quality/Quality.jsx
+++ src/components/Quality/Quality.jsx
@@ -1,38 +1,51 @@
-import React from 'react'
 import './Quality.css'
 import ringW from '../../assets/images/tall.jpg'
 import ringH from '../../assets/images/wommen.png'
 import ringR from '../../assets/images/ringNM.png'
+import { useEffect } from 'react'
+import Aos from 'aos'
 
 export default function Quality() {
-  return (
-    <section className='Quality'>
-        <div className="side1">
-            <img className='img1' src={ringW} alt="" />
-            <div className="para1">
-                <p>01.</p>
-                <p>ELEGANCE</p>
-            </div>
-        </div>
-        <div className="side2">
-            <div className="item">
+    useEffect(() => {
+        Aos.init({
+            duration: 1000,
+        })
+    }, []);
+
+    return (
+        <section className='Quality'>
+            <div className="side1">
+                <img className='img1'
+                    src={ringW} alt="The ring on the bride's hand"
+                    data-aos="fade-right" />
                 <div className="para1">
-                    <p>02.</p>
-                    <p>QUALITY</p>
+                    <p>01.</p>
+                    <p>ELEGANCE</p>
                 </div>
-                <img src={ringH} alt="" />
             </div>
-            <div className="item">
-                <div className="para1">
-                    <p>03.</p>
-                    <p>QUALITY</p>
+            <div className="side2">
+                <div className="item">
+                    <div className="para1">
+                        <p>02.</p>
+                        <p>QUALITY</p>
+                    </div>
+                    <img src={ringH}
+                        alt="The ring is on the finger"
+                        data-aos="fade-left" />
                 </div>
-                <img src={ringR} alt="" />
+                <div className="item">
+                    <div className="para1">
+                        <p>03.</p>
+                        <p>QUALITY</p>
+                    </div>
+                    <img src={ringR}
+                        alt="The gemstone of the ring is magnified"
+                        data-aos="fade-left" />
+                </div>
+                <p>
+                    Crafted with meticulous precision, each ring is designed to meet the highest standards to make it a timelessly elegant ring.
+                </p>
             </div>
-            <p>
-                Crafted with meticulous precision, each ring is designed to meet the highest standards to make it a timelessly elegant ring.
-            </p>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
